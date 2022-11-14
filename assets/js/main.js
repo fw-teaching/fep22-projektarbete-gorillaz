9ae215b9cb7b60299fe9a6349a0e99a4805d8344
= <=>
2022-11-14 14:14:59 +0200

    Sätter till nödvändiga filer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
-/* Site-wide JS i den här filen (t.ex. huvudmenyn) */
+console.log('main.js init');
 
-console.log('main.js init'); // För att se att skriptet laddats in
+const products = [
+    {name: "dildo", price: 69.90, stock: 100},
+    {name: "amongus", price: 1069.90, stock: 45},
+    {name: "piss", price: 68, stock: 60}
+];
+
+for (let i = 0; i < products.length; i++){
+    console.log(`En ${products[i].name} kostar ${products[i].price}€ och det finns ${products[i].stock} stycken kvar av dom`)
+
+    /*document.querySelector("#products").innerHTML += `
+        <li onclick="alert('vi har ${products[i].stock} på lager')">
+            ${products[i].name}: ${products[i].price}
+        </li>`*/
+}
