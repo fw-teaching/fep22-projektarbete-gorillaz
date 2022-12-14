7b2f99e9c8e9ed01c5afa6fab75fb3ac75b3c9b6
= <=>
2022-12-14 22:50:55 +0200

    nav working, gallery halfway done, htmls filled

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,30 +10,6 @@ function openNav() {
     document.getElementById("mySidebar").style.width = "0";
   }
 
-const products = [
-    {name: "dildo", price: 69.90, stock: 100},
-    {name: "amongus", price: 1069.90, stock: 45},
-    {name: "piss", price: 68, stock: 60}
-];
-
-for (let i = 0; i < products.length; i++){
-    console.log(`En ${products[i].name} kostar ${products[i].price}€ och det finns ${products[i].stock} stycken kvar av dom`)
-
-    /*document.querySelector("#products").innerHTML += `
-        <li onclick="alert('vi har ${products[i].stock} på lager')">
-            ${products[i].name}: ${products[i].price}
-        </li>`*/
-}
-/* Kommenterar bort för det är ett helvete att följa med console xd
-let count = 0;
-const myTimer = setInterval(function () {
-    console.log("Hello", count)
-    count++;
-}, 1000)
-
-function stopHello() {
-    clearInterval(myTimer);
-}*/
 function handleUserData() {
     var print = document.getElementById("gibData");
     var appVer = navigator.appVersion;
