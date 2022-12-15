d32619114b294b0bcd554f3c8177f4166bd9a074
= <=>
2022-12-15 18:45:23 +0200

    Cleanup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,15 @@
 console.log('main.js init');
 
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+
+/* SIDEBAR */
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
   }
   
-  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
   function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
   }
+
 
   //DOES NOT WORK
 function ageVerification(){
